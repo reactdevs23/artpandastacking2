87c5f24b409d588607f171be0d83118ee6e66b3a
dalimkumar604 <dalimkumar212582@gmail.com>
2023-03-27 23:43:29 +0600

    askd

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HistoryOfStacking from "../../components/HistoryOfStacking/HistoryOfStacking";
 import Staking from "../../components/Staking/Staking";
-import styles from "./Home.module.css";
+import styles from "./styles.module.css";
 
 const Home = () => {
   return (
