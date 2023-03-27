5d32e280611a81e1de623e8839cf9421823f4967
dalimkumar604 <dalimkumar212582@gmail.com>
2023-03-27 23:08:20 +0600

    hello

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.myContainer}>
       {" "}
       <div className={styles.wrapper}>
         <HistoryOfStacking />
